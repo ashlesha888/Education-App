b05884e0755187b556ac9004f39c92d677551a2b
ashlesha888 <ashleshagautam1983@gmail.com>
2026-07-01 19:49:27 +0530

    added update-section funtion

--- controllers/sectionController.js
+++ controllers/sectionController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Course from "../models/Course.js";
 import Section from "../models/Section.js";
 import Subsection from "../models/Subsection.js";
@@ -106,6 +107,58 @@ export const createSubsection = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Failed to create subsection",
+    });
+  }
+};
+
+
+export const updateSection = async (req, res) => {
+  try {
+    const { sectionId, sectionName } = req.body;
+
+    if (!sectionId || !sectionName) {
+      return res.status(400).json({
+        success: false,
+        message: "Section ID and Section Name are required",
+      });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(sectionId)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Section ID",
+      });
+    }
+
+    const updatedSection = await Section.findByIdAndUpdate(
+      sectionId,
+      {
+        sectionName,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedSection) {
+      return res.status(404).json({
+        success: false,
+        message: "Section not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: updatedSection,
+      message: "Section updated successfully",
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Failed to update section",
     });
   }
 };
