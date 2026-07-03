2e65cf05b9553e3c8eeb930c1d0ad170ffde3ab2
ashlesha888 <ashleshagautam1983@gmail.com>
2026-07-03 19:18:54 +0530

    feat: add instructor dashboard controller and helper functions for course management

---
+++ controllers/controllers/instructorDashboardController.js
@@ -0,0 +1,24 @@
+export const getInstructorDashboard = async (req, res) => {};
+
+export const getInstructorCourses = async (req, res) => {};
+
+export const getCourseStatistics = async (req, res) => {};
+
+export const getStudentCount = async (req, res) => {};
+
+export const getRevenue = async (req, res) => {};
+
+export const getAverageRatings = async (req, res) => {};
+
+export const getDashboardSummary = async (req, res) => {};
+
+export const getRecentEnrollments = async (req, res) => {};
+
+export const getTopPerformingCourses = async (req, res) => {};
+
+export const getMonthlyRevenue = async (req, res) => {};
+
+export const getMonthlyEnrollments = async (req, res) => {};
+
+export const getCourseCompletionStatistics = async (req, res) => {};
+
---
+++ utils/instructorDashboardHelper.js
@@ -0,0 +1,4 @@
+import Course from "../models/Course.js";
+import User from "../models/User.js";
+import CourseProgress from "../models/courseProgress.js";
+import RatingAndReview from "../models/ratingAndReview.js";
