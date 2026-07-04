6bd24c03b54eb0339d9626024fc81bcf625a31f1
ashlesha888 <ashleshagautam1983@gmail.com>
2026-07-04 20:10:16 +0530

    chore: remove placeholder comment in studentDashboardHelper.js

--- utils/studentDashboardHelper.js
+++ utils/studentDashboardHelper.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js";
 import CourseProgress from "../models/CourseProgress.js";
-//next step with code please
+
 export const fetchPurchasedCourses = async (
   studentId
 ) => {
