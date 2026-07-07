f1569540d575db3e06a376d607dc666207ce48b0
ashlesha888 <ashleshagautam1983@gmail.com>
2026-07-07 18:36:34 +0530

    refactor: simplify formatting structure in formatUploadedFile function

--- utils/fileFormatter.js
+++ utils/fileFormatter.js
@@ -6,34 +6,15 @@ export const formatUploadedFile = (
 ) => {
 
   return {
-    publicId:
-      uploadResult.public_id,
-
-    url:
-      uploadResult.secure_url,
-
-    resourceType:
-      uploadResult.resource_type,
-
-    format:
-      uploadResult.format,
-
-    width:
-      uploadResult.width,
-
-    height:
-      uploadResult.height,
-    
-    duration:
-      uploadResult.duration || null,
-
-    originalFilename:
-    uploadResult.original_filename,
-
-    size:
-      uploadResult.bytes,
-
-    createdAt:
-      uploadResult.created_at,
-  };
+    publicId: uploadResult.public_id,
+    url: uploadResult.secure_url,
+    resourceType: uploadResult.resource_type,
+    format: uploadResult.format,
+    width: uploadResult.width,
+    height: uploadResult.height,    
+    duration: uploadResult.duration || null,
+    originalFilename: uploadResult.original_filename,
+    size: uploadResult.bytes,
+    createdAt: uploadResult.created_at,  
+    };
 };
