47971c05ccb34267042e221659f1e7327601c34e
ashlesha888 <ashleshagautam1983@gmail.com>
2026-07-07 18:08:12 +0530

    refactor: streamline uploader creation and enhance file limit configuration

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,58 +1,7 @@
 import multer from "multer";
-import { FILE_LIMITS } from "./config/constants.js";
-
-const storage =
-    multer.memoryStorage();
-
-const upload =multer({
-        storage,
-
-        limits: {
-            fileSize:
-            FILE_LIMITS.IMAGE
-        }
-});
-
-export default Object.freeze(
-  upload
-);
-
-
+import { FILE_LIMITS } from "../config/constants.js";
 
 const storage = multer.memoryStorage();
-
-
-export const createUploader = ({
-  fileSize,
-}) => {
-  return multer({
-    storage,
-
-    limits: {
-      fileSize,
-    },
-  });
-};
-
-export const uploadImage = Object.freeze(
-  createUploader({
-    fileSize:
-      FILE_LIMITS.IMAGE,
-  })
-);
-
-export const uploadVideo =Object.freeze(
-  createUploader({fileSize:
-      FILE_LIMITS.VIDEO,
-  })
-);
-
-export const uploadDocument =Object.freeze(
-  createUploader({
-    fileSize:
-      FILE_LIMITS.DOCUMENT,
-  })
-);
 
 export const createUploader = ({
   fileSize,
@@ -60,10 +9,27 @@ export const createUploader = ({
 }) => {
   return multer({
     storage,
-
     limits: {
       fileSize,
       files,
     },
   });
 };
+
+export const uploadImage = Object.freeze(
+  createUploader({
+    fileSize: FILE_LIMITS.IMAGE,
+  })
+);
+
+export const uploadVideo = Object.freeze(
+  createUploader({
+    fileSize: FILE_LIMITS.VIDEO,
+  })
+);
+
+export const uploadDocument = Object.freeze(
+  createUploader({
+    fileSize: FILE_LIMITS.DOCUMENT,
+  })
+);
