39a59167bdfbc8a4c0f07681a41dc58ca89a3a80
ashlesha888 <ashleshagautam1983@gmail.com>
2026-07-10 16:20:15 +0530

    feat: add express-validator for email and password validation; implement validation middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,14 @@
 import express from "express";
 import { sendOTP, signUp, login, logout, forgotPassword, resetPassword, changePassword } from "../controllers/authController.js";
 import { auth, isStudent, isInstructor, isAdmin } from "../middlewares/auth.js";
+import {
+  emailValidation,
+  passwordValidation,
+} from "../validators/authValidator.js";
+
+import {
+  validateRequest,
+} from "../middlewares/validationMiddleware.js";
 
 const router = express.Router();
 
@@ -39,5 +47,18 @@ router.get("/admin-portal", auth, isAdmin, (req, res) => {
     message: "Welcome to the protected Admin Portal!",
   });
 });
+router.post(
+
+  "/signup",
+
+  emailValidation,
+
+  passwordValidation,
+
+  validateRequest,
+
+  signUp
+
+);
 
 export default router;
---
+++ validators/authValidator.js
@@ -0,0 +1,75 @@
+import { body } from "express-validator";
+
+export const emailValidation = [
+
+  body("email")
+
+    .trim()
+
+    .notEmpty()
+
+    .withMessage("Email is required.")
+
+    .isEmail()
+
+    .withMessage("Invalid email address.")
+
+    .normalizeEmail(),
+
+];
+export const passwordValidation = [
+
+  body("password")
+
+    .notEmpty()
+
+    .withMessage("Password is required.")
+
+    .isLength({
+      min: 8,
+    })
+
+    .withMessage(
+      "Password must contain at least 8 characters."
+    )
+
+    .matches(/[A-Z]/)
+
+    .withMessage(
+      "Password must contain one uppercase letter."
+    )
+
+    .matches(/[a-z]/)
+
+    .withMessage(
+      "Password must contain one lowercase letter."
+    )
+
+    .matches(/[0-9]/)
+
+    .withMessage(
+      "Password must contain one number."
+    )
+
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+
+    .withMessage(
+      "Password must contain one special character."
+    ),
+
+];
+import { param } from "express-validator";
+
+export const objectIdValidation = (
+  field
+) => [
+
+  param(field)
+
+    .isMongoId()
+
+    .withMessage(
+      `Invalid ${field}.`
+    ),
+
+];
